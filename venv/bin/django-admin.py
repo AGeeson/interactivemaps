c824180710532c08227d83a8a6f7dab84d275621
Alex Geeson <alexanderwgeeson@gmail.com>
2020-03-30 19:00:02 +1030

    help

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alexandergeeson/Github/OutdoorX_Working/outdoorx_googlemaps copy 14.03.20/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
